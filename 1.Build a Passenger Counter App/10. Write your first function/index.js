b98fb79dc6eb914caf2f8072b22cb57b64a4c334
Swaraj Kumar Mohapatra <102534144+swarajkm@users.noreply.github.com>
2022-07-05 13:12:51 +0530

    functions in js

--- 1.Build a Passenger Counter App/10. Write your first function/index.js
+++ 1.Build a Passenger Counter App/10. Write your first function/index.js
@@ -1,3 +1,8 @@
 // Create a function (you decide the name) that logs out the number 42 to the console
 // Call/invoke the function
 
+function numfortytwo() {
+  console.log("42");
+}
+
+numfortytwo();
--- 1.Build a Passenger Counter App/12. Write a function that increments/index.js
+++ 1.Build a Passenger Counter App/12. Write a function that increments/index.js
@@ -1,9 +1,13 @@
-let lapsCompleted = 0
+let lapsCompleted = 0;
 
 // Create a function that increments the lapsCompleted variable with one
 // Run it three times
 
+function incrementLap() {
+  lapsCompleted = lapsCompleted + 1;
+}
 
-
-
-console.log(lapsCompleted)
+incrementLap();
+incrementLap();
+incrementLap();
+console.log(lapsCompleted);
