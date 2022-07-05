ed03484e03db3c5ea9f16a9d65104895f534860d
Swaraj Kumar Mohapatra <102534144+swarajkm@users.noreply.github.com>
2022-07-05 13:33:22 +0530

    increment onclick button

--- 1.Build a Passenger Counter App/14. Display the count/index.js
+++ 1.Build a Passenger Counter App/14. Display the count/index.js
@@ -2,11 +2,12 @@
 
 // change the count-el in the HTML to reflect the new count
 
-let count = 0
+let countEl = document.getElementById("count-el");
+
+let count = 0;
 
 function increment() {
-    count = count + 1
-    console.log(count)
+  count = count + 1;
+  countEl.innerText = count;
+  console.log(count);
 }
-
-
