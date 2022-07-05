a45f0e04bb5074c84e9cc614a18decc3e20fe9f1
Swaraj Kumar Mohapatra <102534144+swarajkm@users.noreply.github.com>
2022-07-05 15:38:34 +0530

    Added previous entry functionality

--- 1.Build a Passenger Counter App/19. Write your first string variable/index.js
+++ 1.Build a Passenger Counter App/19. Write your first string variable/index.js
@@ -1,4 +1,9 @@
-let username = 'per"
+let username = "per";
+//can use both single as well as double quotes
+console.log(username);
 
-console.log(username)
+let msg = "You have three new notifications";
+console.log(msg + " " + username);
 
+let userMsg = msg + ", " + username + "!";
+console.log(userMsg);
